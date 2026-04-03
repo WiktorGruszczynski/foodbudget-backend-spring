2dbfec43301a196e850ea409b178f89c57c9639e
WiktorGruszczynski <wgwiktorwg@gmail.com>
2026-04-03 23:22:10 +0200

    fix: logging

--- src/main/java/org/example/foodbudgetbackendspring/config/GlobalExceptionHandler.java
+++ src/main/java/org/example/foodbudgetbackendspring/config/GlobalExceptionHandler.java
@@ -1,11 +1,13 @@
 package org.example.foodbudgetbackendspring.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
@@ -13,7 +15,7 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ApiError> handleRuntime(
             RuntimeException ex
     ){
-        System.err.println("Business error: " + ex.getMessage());
+        log.error("Business error", ex);
         ApiError error = new ApiError(
                 LocalDateTime.now(),
                 ex.getMessage()
